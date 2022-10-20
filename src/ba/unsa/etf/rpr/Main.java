d61b787a8c69e9ba90c11b387c6651cf2ee02e34
lheleg <lheleg1@etf.unsa.ba>
2022-10-20 23:35:14 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,43 @@
+package ba.unsa.etf.rpr;
+
+class Sat{
+    private int sati,minute,sekunde;
+    Sat(int sati, int minute, int sekunde) {
+        this.sati = sati;
+        this.minute = minute;
+        this.sekunde = sekunde;
+    }
+    void Postavi(int sati, int minute, int sekunde) { this.sati=sati; this.minute=minute; this.sekunde=sekunde; }
+    void Sljedeci() {
+        sekunde++;
+        if (sekunde==60) { sekunde=0; minute++; }
+        if (minute==60) { minute=0; sati++; }
+        if (sati==24) sati=0;
+    }
+    void Prethodni() {
+        sekunde--;
+        if (sekunde==-1) { sekunde=59; minute--; }
+        if (minute==-1) { minute=59; sati--; }
+        if (sati==-1) sati=23;
+    }
+    void PomjeriZa(int pomak) {
+        if (pomak>0) for (int i=0; i<pomak; i++) Sljedeci();
+		else for (int i=0; i<-pomak; i++) Prethodni();
+    }
+    int DajSate(){ return sati; }
+    int DajMinute(){ return minute; }
+    int DajSekunde(){ return sekunde; }
+    void Ispisi(){ System.out.println(sati+":"+minute+":"+sekunde);}
+}
+public class Main {
+public static void main(String[] args) {
+    Sat s= new Sat(15, 30, 45);
+    s.Ispisi();
+    s.Sljedeci();
+    s.Ispisi();
+    s.PomjeriZa(-48);
+    s.Ispisi();
+    s.Postavi(0, 0, 0);
+    s.Ispisi();
+    }
+}
